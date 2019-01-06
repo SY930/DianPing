d2174f47677b2db16c10f86d78d6b89c982b6716
SY930 <867598193@qq,com>
2019-01-06 18:13:50 +0800

    开发商家详情页

--- app/components/List/Item/index.jsx
+++ app/components/List/Item/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
+import { Link } from 'react-router'
 import './style.less'
 
 class ListItem extends React.Component {
@@ -10,8 +10,10 @@ class ListItem extends React.Component {
     }
     render() {
         const data = this.props.data
+        console.log(data);
         return (
             <div className="list-item clear-fix">
+                <Link to={'/detail/' + data.id}>
                 <div className="item-img-container float-left">
                     <img src={data.img} alt={data.title}/>
                 </div>
@@ -28,6 +30,7 @@ class ListItem extends React.Component {
                         <span className="mumber float-right">已售{data.mumber}</span>
                     </div>
                 </div>
+                </Link>
             </div>
         )
     }
